9aa31b056da97f4a2544656f2f0066043b7f7a62
andy <anz4corp@proton.me>
2026-04-18 12:05:39 +0700

    minor fix get linux platform release

--- chrome_release.py
+++ chrome_release.py
@@ -1,7 +1,7 @@
 import json
 import urllib.request
 
-url = "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=&num=1&offset=0"
+url = "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=linux&num=1&offset=0"
 response = urllib.request.urlopen(url)
 data = response.read().decode("utf-8")
 
